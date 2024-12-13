ec9896a503966390bf4baf21aa8613fba0a5d1ed
Nagesh-DJ <nageshdj.spider@gmail.com>
2024-12-13 10:28:23 +0530

    added new stmts

--- demoProject/src/test/java/pack2/CreateOrgTest.java
+++ demoProject/src/test/java/pack2/CreateOrgTest.java
@@ -6,6 +6,15 @@ public class CreateOrgTest {
 @Test(groups = "SmokeTesting")
 public void createOrgTest()
 	{
+	String Browser=System.getProperty("browser");
+	String url=System.getProperty("url");
+	String username=System.getProperty("username");
+	String password=System.getProperty("password");
+	System.out.println("browser: "+Browser);
+	System.out.println("url: "+url);
+	System.out.println("username: "+username);
+	System.out.println("password: "+password);
+
 	System.out.println("execute createOrgTest");
 	}
 
